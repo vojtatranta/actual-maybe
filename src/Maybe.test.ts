1a74e02f8aa8ad3435463a4a53c68eb8de23e6b4
Vojtěch Tranta <vojta.tranta@gmail.com>
2024-01-24 20:11:30 +0100

    add test for asInput function reference keeping

--- src/Maybe.test.ts
+++ src/Maybe.test.ts
@@ -1,152 +1,177 @@
-import { Maybe } from './Maybe'
-
+import { Maybe } from "./Maybe";
 
 class A {}
 
 class B {}
 
 const getNullableA = (): A | null => {
-  return null
-}
+  return null;
+};
 
 const getNullableB = (): B | null => {
-  return null
-
-}
+  return null;
+};
 
 const bToNullable = (b: B): B | null => {
-  return null
-}
+  return null;
+};
 
 describe("Maybe", () => {
+  describe("getting original or default value", () => {
+    it("should return null when Maybe is A | null | undefined", () => {
+      const a = getNullableA();
 
-    describe("getting original or default value", () => {
-        it("should return null when Maybe is A | null | undefined", () => {
-            const a = getNullableA()
+      const result: A | null | undefined = Maybe.of(a).getValue();
+      expect(result).toEqual(null);
+    });
 
-            const result: A | null | undefined = Maybe.of(a).getValue()
-            expect(result).toEqual(null)
-        })
-        
-        it("should return undefined when default null parameter is passed to getValue()", () => {
-            const a = getNullableA()
+    it("should return undefined when default null parameter is passed to getValue()", () => {
+      const a = getNullableA();
 
-            const result: A | null = Maybe.of(a).getValue(null)
-            expect(result).toEqual(null)
-        })
+      const result: A | null = Maybe.of(a).getValue(null);
+      expect(result).toEqual(null);
+    });
 
-        it("should return default when default parameter is passed to getValue()", () => {
-            const a = getNullableA()
-            const b = "b" as const
+    it("should return default when default parameter is passed to getValue()", () => {
+      const a = getNullableA();
+      const b = "b" as const;
 
-            const result: A | "b" = Maybe.of(a).getValue(b)
-            expect(result).toEqual("b")
-        })
+      const result: A | "b" = Maybe.of(a).getValue(b);
+      expect(result).toEqual("b");
+    });
 
-        it("should return original when default parameter is passed to getValue()", () => {
-            const a = "a" as const
-            const b = "b" as const
+    it("should return original when default parameter is passed to getValue()", () => {
+      const a = "a" as const;
+      const b = "b" as const;
 
-            const result: "a" | "b" = Maybe.of(a).getValue(b)
-            expect(result).toEqual("a")
-        })
+      const result: "a" | "b" = Maybe.of(a).getValue(b);
+      expect(result).toEqual("a");
+    });
 
-        it("should return original when default parameter is NOT passed to getValue()", () => {
-            const a = "a" as const
+    it("should return original when default parameter is NOT passed to getValue()", () => {
+      const a = "a" as const;
 
-            const result: "a" | undefined | null = Maybe.of(a).getValue()
-            expect(result).toEqual("a")
-        })
+      const result: "a" | undefined | null = Maybe.of(a).getValue();
+      expect(result).toEqual("a");
+    });
 
-        it("should return original when default parameter is passed to getValue() with null", () => {
-            const a = "a" as const
+    it("should return original when default parameter is passed to getValue() with null", () => {
+      const a = "a" as const;
 
-            const result: "a" | null = Maybe.of(a).getValue(null)
-            expect(result).toEqual("a")
-        })
+      const result: "a" | null = Maybe.of(a).getValue(null);
+      expect(result).toEqual("a");
+    });
 
-        describe("Maybe#all()", () => {
-            it("should resolve all() values as results", (callback) => {
-                Maybe.all(["a" as const, "b" as const]).map(([a, b]: ["a", "b"]) => {
-                    expect(a).toEqual("a")
-                    expect(b).toEqual("b")
-                    callback()
-                })
-            })
+    describe("Maybe#all()", () => {
+      it("should resolve all() values as results", (callback) => {
+        Maybe.all(["a" as const, "b" as const]).map(([a, b]: ["a", "b"]) => {
+          expect(a).toEqual("a");
+          expect(b).toEqual("b");
+          callback();
+        });
+      });
 
-            it("should resolve all() maybies as results", (callback) => {
-                Maybe.all([Maybe.of("a" as const), Maybe.of("b" as const)]).map(([a, b]: ["a", "b"]) => {
-                    expect(a).toEqual("a")
-                    expect(b).toEqual("b")
-                    callback()
-                })
-            })
+      it("should resolve all() maybies as results", (callback) => {
+        Maybe.all([Maybe.of("a" as const), Maybe.of("b" as const)]).map(
+          ([a, b]: ["a", "b"]) => {
+            expect(a).toEqual("a");
+            expect(b).toEqual("b");
+            callback();
+          }
+        );
+      });
 
-            it("should resolve all() some values and some mabies and upack them as results", (callback) => {
-                Maybe.all(["a" as const, Maybe.of("b" as const)]).map(([a, b]: ["a", "b"]) => {
-                    expect(a).toEqual("a")
-                    expect(b).toEqual("b")
-                    callback()
-                })
-            })
+      it("should resolve all() some values and some mabies and upack them as results", (callback) => {
+        Maybe.all(["a" as const, Maybe.of("b" as const)]).map(
+          ([a, b]: ["a", "b"]) => {
+            expect(a).toEqual("a");
+            expect(b).toEqual("b");
+            callback();
+          }
+        );
+      });
 
-            it("should NOT resolve all() because some value is nullable", (callback) => {
-                const value = Maybe.all(["a" as const, Maybe.of("b" as const), null]).map(() => {
-                    callback.fail()
-                }).getValue("c" as const)
+      it("should NOT resolve all() because some value is nullable", (callback) => {
+        const value = Maybe.all(["a" as const, Maybe.of("b" as const), null])
+          .map(() => {
+            callback.fail();
+          })
+          .getValue("c" as const);
 
-                expect(value).toEqual("c")
-                callback()
-            })
-        })
-    })
+        expect(value).toEqual("c");
+        callback();
+      });
+    });
+  });
 
-    describe("type tests", () => {
-        
+  describe("type tests", () => {
     it("basic Maybe of A", () => {
-        const result: Maybe<A>  = Maybe.of(getNullableA())
-    })
+      const result: Maybe<A> = Maybe.of(getNullableA());
+    });
 
     it("string or null", () => {
-        const result: null | string  = Maybe.of("ahoj").getValue(null)
-    })
+      const result: null | string = Maybe.of("ahoj").getValue(null);
+    });
 
     it("string or undefined", () => {
-        const result: undefined | string | null  = Maybe.of("ahoj").getValue()
-    })
+      const result: undefined | string | null = Maybe.of("ahoj").getValue();
+    });
 
     it("nullable or B", () => {
-        const result: B | "ahoj" = Maybe.of(getNullableB()).map(b => bToNullable(b)).getValue("ahoj" as const)
-    })
+      const result: B | "ahoj" = Maybe.of(getNullableB())
+        .map((b) => bToNullable(b))
+        .getValue("ahoj" as const);
+    });
 
     describe("map", () => {
-        it("B to A through map()", () => {
-            const result: Maybe<A> = Maybe.of(getNullableB()).map(() => getNullableA())
-        })
-    })
+      it("B to A through map()", () => {
+        const result: Maybe<A> = Maybe.of(getNullableB()).map(() =>
+          getNullableA()
+        );
+      });
+    });
 
     describe("flatMap", () => {
-        it("B to A through flatMap() through 'eating Maybe<A>'", () => {
-            const result: Maybe<A> = Maybe.of(getNullableB()).flatMap(() => Maybe.of(getNullableA()))
-        })
+      it("B to A through flatMap() through 'eating Maybe<A>'", () => {
+        const result: Maybe<A> = Maybe.of(getNullableB()).flatMap(() =>
+          Maybe.of(getNullableA())
+        );
+      });
 
-        it("B to A through flatMap()", () => {
-            const result: Maybe<A> = Maybe.of(getNullableB()).flatMap(() => getNullableA())
-        })
-    })
-
-
+      it("B to A through flatMap()", () => {
+        const result: Maybe<A> = Maybe.of(getNullableB()).flatMap(() =>
+          getNullableA()
+        );
+      });
+    });
 
     describe("as input function", () => {
-        it("string to Maybe<number> through asInput()", () => {
-            const inputMaybeFunc =  Maybe.asInput((input: string) => input.length)
-            const result: Maybe<number> = inputMaybeFunc("test")
-        })
+      it("string to Maybe<number> through asInput()", () => {
+        const inputMaybeFunc = Maybe.asInput((input: string) => input.length);
+        const result: Maybe<number> = inputMaybeFunc("test");
+      });
 
-        it("string to Maybe<number> through asMaybeInput()", () => {
-            const inputAsMaybeFunc =  Maybe.asMaybeInput((maybeInput: Maybe<string>) => maybeInput.map((str) => str.length))
-            const result: Maybe<number> = inputAsMaybeFunc("test")
-        })
-    })
-    })
-})
+      it("should keep the reference to the originally defined as input function", () => {
+        class ANumber {
+          constructor(public value: number) {
+            this.value = value;
+          }
+        }
+        const inputMaybeFunc = Maybe.asInput(
+          (input: string) => new ANumber(input.length)
+        );
+        const result1: Maybe<ANumber> = inputMaybeFunc("test");
+        const result2: Maybe<ANumber> = inputMaybeFunc("test");
+        expect(inputMaybeFunc).toBe(inputMaybeFunc);
+        expect(result1).not.toBe(result2);
+      });
+
+      it("string to Maybe<number> through asMaybeInput()", () => {
+        const inputAsMaybeFunc = Maybe.asMaybeInput(
+          (maybeInput: Maybe<string>) => maybeInput.map((str) => str.length)
+        );
+        const result: Maybe<number> = inputAsMaybeFunc("test");
+      });
+    });
+  });
+});
